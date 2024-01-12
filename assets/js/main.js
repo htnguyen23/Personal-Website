bfd7125a57492b035d6a2dbc03dc5cc817df3b94
Huong <htnguyen23@wisc.edu>
2024-01-12 17:05:56 -0600

    Branch to compile and edit experience and projects.

--- assets/js/main.js
+++ assets/js/main.js
@@ -109,6 +109,35 @@ linkProject.forEach(l => l.addEventListener('click', activeProject))
 
 /*=============== LIGHT DARK THEME ===============*/ 
 
+const themeButton = document.getElementById('theme-button')
+const lightTheme = 'light-theme'
+const iconTheme = 'bx-sun'
+
+// previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// we obtain the current theme that the interface has by validating the light-theme class
+const getCurrentTheme = () => document.body.classList.contains(lightTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun'
+
+// we validate if the user previously chose a topic
+if (selectedTheme) {
+    // if the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the light
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](lightTheme)
+    themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// activate/deactivate the theme manually w button
+themeButton.addEventListener('click', () => {
+    // add or remove the light/icon theme
+    document.body.classList.toggle(lightTheme)
+    themeButton.classList.toggle(iconTheme)
+    // save the theme and the current icon user chose
+    localStorage.setItem('selected-theme', getCurrentTheme());
+    localStorage.setItem('selected-icon', getCurrentIcon());
+})
+
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
