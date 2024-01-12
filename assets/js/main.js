e9dc5c14716522d01a4b834f9786f94347464762
Huong <htnguyen23@wisc.edu>
2024-01-11 21:06:00 -0600

    setup SKILLS section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,15 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader(){
+    const header = document.getElementById('header')
 
+    // when the scroll is greater than 50 viewport height, add the scroll-header to the header tag
+    if (this.scrollY >= 50) {
+        header.classList.add('scroll-header');
+    } else {
+        header.classList.remove('scroll-header')
+    }
+    window.addEventListener('scroll', scrollHeader)
+}
 
 /*=============== SERVICES MODAL ===============*/
 
